0023a4429964f22475e47b7e50a0eef04fa3abba
singh.chand <singh.chand@netcorecloud.com>
2025-02-04 05:10:03 +0530

    updated SpeechLowFreq

--- constants/constants.go
+++ constants/constants.go
@@ -3,6 +3,6 @@ package constants
 const (
 	SampleRate     = 44100.0 // Sample rate in Hz
 	FFTWindowSize  = 2048    // FFT window size (46ms window at 44100Hz)
-	SpeechLowFreq  = 300.0   // Lower frequency threshold for speech detection
+	SpeechLowFreq  = 250.0   // Lower frequency threshold for speech detection
 	SpeechHighFreq = 4000.0  // Upper frequency threshold for speech detection
 )
